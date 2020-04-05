6b6a14dedd95ffe0e0396ef5bb61effa3cd58c2b
harrisonvanderbyl <33416119+harrisonvanderbyl@users.noreply.github.com>
2020-04-06 00:11:07 +1000

    corrected wrong dependency

--- gpsmodule/config.py
+++ gpsmodule/config.py
@@ -3,7 +3,7 @@ def can_build(plat):
 
 def configure(env):
 	if (env['platform'] == 'android'):
-		env.android_add_dependency("compile ('com.google.android.gms:play-services-ads:16.0.0') { exclude group: 'com.android.support' }")
+		env.android_add_dependency("compile ('com.google.android.gms:play-services-location:16.0.0') { exclude group: 'com.android.support' }")
 		env.android_add_java_dir("android")
 		env.android_add_to_manifest("android/AndroidManifestChunk.xml")
 		env.disable_module()
